5338e1661a655f2f5e5e959a5f8a8bb40dfb46bf
chezass <71591247+chezass@users.noreply.github.com>
2021-02-19 01:59:42 +0300

    Dungeon Quest (v2.0 Unity pre-alpha)
    Перенос кода с Visual Studio на движок Unity (ОПЯТЬ РЕИНЖИНИРИНГ ММММ ОБОЖАЮ)
    
    Добавлено:
    - почти работающее главное меню;
    Всё...пока что)

---
+++ Dungeon Quest (v2.0 Unity pre-alpha)/Assets/Scripts/OpenAboutPanel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenAboutPanel : MonoBehaviour
+{
+    public GameObject AboutPanel;
+    public void OpenAbout()
+    {
+        if (AboutPanel != null)
+        {
+            bool isActive = AboutPanel.activeSelf;
+            AboutPanel.SetActive(!isActive);
+        }
+    }
+
+
+}
+
---
+++ Dungeon Quest (v2.0 Unity pre-alpha)/Assets/Scripts/OpenExitPanel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenExitPanel : MonoBehaviour
+{
+    public GameObject ExitPanel;
+    public void OpenExit()
+    {
+        if (ExitPanel != null)
+        {
+            bool isActive = ExitPanel.activeSelf;
+            ExitPanel.SetActive(!isActive);
+        }
+    }
+
+}
